7063accbc7f1fa72c8be5fcadaf466af3c82d62a
rpanchyk <ruslan.panchik@gmail.com>
2019-02-09 13:38:32 +0200

    Add API prototype

--- main.go
+++ main.go
@@ -8,12 +8,26 @@ import (
 func main() {
 	router := httprouter.New()
 
+	// UI
 	router.GET("/", serveIndex)
 	router.ServeFiles("/static/*filepath", http.Dir("static"))
+	// API
+	router.POST("/api", serveEncode)
+	router.GET("/api", serveDecode)
 
 	http.ListenAndServe(":8080", router)
 }
 
 func serveIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	http.ServeFile(w, r, "static/index.html")
+}
+
+func serveEncode(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	responseBytes := []byte("{\"key\":\"value\"}")
+	w.Write(responseBytes)
+}
+
+func serveDecode(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	responseBytes := []byte("{\"key\":\"value\"}")
+	w.Write(responseBytes)
 }
