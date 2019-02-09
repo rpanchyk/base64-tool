08cd8ee2ca2b12585235279a1f1fe2878886913a
rpanchyk <ruslan.panchik@gmail.com>
2019-02-09 13:11:25 +0200

    Add backend prototype

--- main.go
+++ main.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"github.com/julienschmidt/httprouter"
+	"net/http"
+)
 
+func main() {
+	//http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static/"))))
+
+	router := httprouter.New()
+
+	router.GET("/", serveIndex)
+	router.ServeFiles("/static/*filepath", http.Dir("static"))
+
+	http.ListenAndServe(":8080", router)
+}
+
+func serveIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	http.ServeFile(w, r, "static/index.html")
 }
